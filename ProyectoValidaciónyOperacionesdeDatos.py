e9435fa593cfe1911293273dd2fa14ffbe50546b
KarenJo-MtzMtlng <kjmartinezmontelongo@gmail.com>
2024-08-26 20:05:06 -0600

    Cambios de detalles

--- ProyectoValidaciónyOperacionesdeDatos.py
+++ ProyectoValidaciónyOperacionesdeDatos.py
@@ -1,4 +1,3 @@
-
 VERDE = "\033[92m"
 CIAN = "\033[96m"
 AMARILLO = "\033[93m"
@@ -6,7 +5,7 @@ RESET = "\033[0m"
 
 print(f"{VERDE}Bienvenido a la Calculadora de Longitud de Palabras{RESET}") #Muestra un mensaje de bienvenida en color.
 print()
-while True: # Inicia un bucle infinito para solicitar palabras hasta que se ingrese una palabra válida.
+while True: #Inicia un bucle infinito para solicitar palabras hasta que se ingrese una palabra válida.
  palabra=input("Ingrese una palabra: ") #Se le pide al usuario que ingrese una palabra.
  x = len(palabra) #Con esta función se mide la longitud de la palabra.
 
@@ -16,7 +15,7 @@ while True: # Inicia un bucle infinito para solicitar palabras hasta que se ingr
  
  elif (x < 4): #Cuando la longitud de la palabra es menor a 4 letras, se realizan las siguientes operaciones.
     faltan = 4 - x #Se hace una resta para calcular cuántas letras faltan para completar un total de cuatro.
-    if faltan == 1 : #Si solo falta una letra, se despliega un mensaje en singular.
+    if faltan == 1: #Si solo falta una letra, se despliega un mensaje en singular.
         print(f"Esta palabra tiene {x} letras, hace falta {faltan} letra.") #Muestra un mensaje indicando que falta una letra para completar el mínimo de cuatro.
     else: #Sino, cuando falta más de una letra, se despliega un mensaje en plural.
         print(f"Esta palabra tiene {x} letras, hacen falta {faltan} letras.") #Muestra un mensaje indicando cuántas letras faltan para alcanzar el mínimo de cuatro cuando faltan más de una.
@@ -28,7 +27,7 @@ while True: # Inicia un bucle infinito para solicitar palabras hasta que se ingr
     else: #Sino, cuando sobra más de una letra, se despliega un mensaje en plural.
         print(f"Esta palabra tiene {x} letras, sobran {sobran} letras.")
 print()
-print(f"{AMARILLO}¡Gracias por usar esta Calculadora de Longitud de Palabras!{RESET}")
+print(f"{AMARILLO}¡Gracias por usar esta Calculadora de Longitud de Palabras!{RESET}") #Mensaje de despedida.
 print()
 
 #Este es el segundo programa
@@ -43,15 +42,15 @@ while True: #Bucle infinito para pedir el valor de X.
 
  if Valor_X==0: #Si el valor de X es 0, se despliega un mensaje que menciona que se encuentra en el origen.
     print(f"El valor {Valor_X} se encuentra en el origen, por favor, ingresa un valor diferente.")
-    continue   #Volver a pedir el valor de X si es 0. Se reinicia el bucle.
+    continue #Volver a pedir el valor de X si es 0. Se reinicia el bucle.
  break #Si el valor es diferente de 0, sale del bucle.
 
 while True: #Bucle infinito para pedir el valor de Y.
  Valor_Y = int(input("Ingrese un valor en Y: ")) #Solicitar al usuario el valor de Y.
 
- if Valor_Y==0:  #Si el valor de Y es 0, se despliega un mensaje que menciona que se encuentra en el origen.
+ if Valor_Y==0: #Si el valor de Y es 0, se despliega un mensaje que menciona que se encuentra en el origen.
    print(f"El valor {Valor_Y} se encuentra en el origen, por favor, ingresa un valor diferente.")
-   continue  #Volver a pedir el valor de Y si es 0. Se reinicia el bucle.
+   continue #Volver a pedir el valor de Y si es 0. Se reinicia el bucle.
  break #Si el valor es diferente de 0, sale del bucle.
 
 print()
@@ -122,5 +121,6 @@ if Valor_X <0 and Valor_Y <0:
   print("                  │   ")
   print("                  │   ")   
   print()
-#Mensaje de despedida.
-print(f"{AMARILLO}¡Gracias por usar esta Identificadora de Cuadrante!{RESET}")
+
+print(f"{AMARILLO}¡Gracias por usar esta Identificadora de Cuadrante!{RESET}") #Mensaje de despedida.
+print()
