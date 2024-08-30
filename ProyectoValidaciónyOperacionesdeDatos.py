96c17a05f16185684867b4ecfbfbbcc3aa17a3fe
KarenJo-MtzMtlng <kjmartinezmontelongo@gmail.com>
2024-08-30 15:07:07 -0600

    Agregar color al primer programa

--- ProyectoValidaciónyOperacionesdeDatos.py
+++ ProyectoValidaciónyOperacionesdeDatos.py
@@ -4,6 +4,7 @@ AMARILLO = "\033[93m"
 RESET = "\033[0m"
 
 #Este es el primer programa
+
 print(f"{VERDE}Bienvenido a la Calculadora de Longitud de Palabras{RESET}") #Muestra un mensaje de bienvenida en color.
 print()
 while True: #Inicia un bucle infinito para solicitar palabras hasta que se ingrese una palabra válida.
@@ -11,22 +12,22 @@ while True: #Inicia un bucle infinito para solicitar palabras hasta que se ingre
  x = len(palabra) #Con esta función se mide la longitud de la palabra.
 
  if (x >= 4 and x <= 8): #Si la palabra tiene mas o igual a cuatro letras y menos o igual a ocho letras, la palabra es correcta.
-    print(f"Esta palabra tiene {x} letras, esta palabra es correcta, buen trabajo!") #Se imprime un mensaje de validación de la palabra.
+    print(f"{CIAN}Esta palabra tiene {x} letras, esta palabra es correcta, buen trabajo!{RESET}") #Se imprime un mensaje de validación de la palabra.
     break #Si se cumple la condición, se rompe el ciclo.
  
  elif (x < 4): #Cuando la longitud de la palabra es menor a 4 letras, se realizan las siguientes operaciones.
     faltan = 4 - x #Se hace una resta para calcular cuántas letras faltan para completar un total de cuatro.
     if faltan == 1: #Si solo falta una letra, se despliega un mensaje en singular.
-        print(f"Esta palabra tiene {x} letras, hace falta {faltan} letra.") #Muestra un mensaje indicando que falta una letra para completar el mínimo de cuatro.
+        print(f"{CIAN}Esta palabra tiene {x} letras, hace falta {faltan} letra.{RESET}") #Muestra un mensaje indicando que falta una letra para completar el mínimo de cuatro.
     else: #Sino, cuando falta más de una letra, se despliega un mensaje en plural.
-        print(f"Esta palabra tiene {x} letras, hacen falta {faltan} letras.") #Muestra un mensaje indicando cuántas letras faltan para alcanzar el mínimo de cuatro cuando faltan más de una.
+        print(f"{CIAN}Esta palabra tiene {x} letras, hacen falta {faltan} letras.{RESET}") #Muestra un mensaje indicando cuántas letras faltan para alcanzar el mínimo de cuatro cuando faltan más de una.
    
  elif (x > 8): #Cuando la longitud de la palabra es mayor a 8 letras, se realizan las siguientes operaciones.
     sobran = x - 8 #Se hace una resta para calcular cuántas letras sobran para completar un total de ocho.
     if sobran == 1: #Si solo sobra una letra, se despliega un mensaje en singular.
-        print(f"Esta palabra tiene {x} letras, sobra {sobran} letra.")
+        print(f"{CIAN}Esta palabra tiene {x} letras, sobra {sobran} letra.{RESET}")
     else: #Sino, cuando sobra más de una letra, se despliega un mensaje en plural.
-        print(f"Esta palabra tiene {x} letras, sobran {sobran} letras.")
+        print(f"{CIAN}Esta palabra tiene {x} letras, sobran {sobran} letras.{RESET}")
 print()
 print(f"{AMARILLO}¡Gracias por usar esta Calculadora de Longitud de Palabras!{RESET}") #Mensaje de despedida.
 print()
